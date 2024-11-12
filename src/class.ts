c7849f2ad532abc38a9659ec97c91c9ffb59ee00
spin-spin-ryo <19991217r@gmail.com>
2024-11-12 23:19:18 +0900

    add: classについて追記

--- src/class.ts
+++ src/class.ts
@@ -15,3 +15,85 @@ export class Person {
 // console.log(p)
 // p.add()
 // console.log(p)
+
+// interface ：基本的にjavaと同じ
+interface Nameable{
+  name:string;
+  nickName:string;
+
+  showFullName():void;
+}
+
+interface NamedPerson extends Nameable{
+  showShortName():void;
+}
+
+class You implements NamedPerson{
+  name: string;
+  nickName: string;
+  constructor(name:string, nickName:string){
+    this.name = name;
+    this.nickName = nickName;
+  }
+
+  showFullName(): void {
+    console.log(this.name)
+  }
+
+  showShortName(): void {
+      console.log(this.nickName)
+  }
+}
+
+const you:Nameable = new You("aaa","bbb")
+you.showFullName()
+// you.showShortName() エラーになる
+
+// readonlyについて
+// 基本読み込み専用になるが, implementsになるとなくなるので注意
+interface Nameable1{
+  name:string;
+  nickName:string;
+
+  showFullName():void;
+}
+
+interface NamedPerson1 extends Nameable1{
+  readonly name:string;
+  showShortName():void;
+}
+
+class You1 implements NamedPerson1{
+  name: string;
+  nickName: string;
+  constructor(name:string, nickName:string){
+    this.name = name;
+    this.nickName = nickName;
+  }
+
+  showFullName(): void {
+    console.log(this.name)
+  }
+
+  showShortName(): void {
+      console.log(this.nickName)
+  }
+}
+
+const you1:Nameable1 = new You1("aaa","bbb")
+you1.showFullName()
+
+
+// interfaceは基本的にtypeでかける
+type NameableType = {
+  name:string,
+  nickName:string,
+  showFullName:() => void
+}
+
+type NamedPersonType = {
+  name:string,
+  nickName:string,
+  showFullName:() => void
+  showShortName: () => void  
+} & NameableType
